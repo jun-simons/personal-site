61ef1edd05a421bc9d2de00e73174b3a41ac43cd
jun-simons <55363743+jun-simons@users.noreply.github.com>
2023-12-07 22:40:56 -0500

    Pushing initial version

---
+++ personal-site/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'sans': ['ui-sans-serif', 'system-ui'],
+      'serif': ['ui-serif', 'Georgia'],
+      'mono': ['ui-monospace', 'SFMono-Regular'],
+      'display': ['PPAcma'],
+      'body': ["Open Sans"],
+    },
+  },
+  plugins: [],
+}
+
