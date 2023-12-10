1e332fd68a15c5dc67b1998291d467a4de6e4c59
jun-simons <55363743+jun-simons@users.noreply.github.com>
2023-12-10 16:05:53 -0500

    namecard version

--- personal-site/src/components/MainContent.js
+++ personal-site/src/components/MainContent.js
@@ -28,13 +28,22 @@ const MainContent = () => {
     },
     "-=1.4")
 
+    t1.to(".card", {
+        minWidth: 100,
+        minHeight: 100,
+        duration: 1.4,
+        ease: "power4.out",
+        delay: 1.4
+    },
+    "-=.8")
+
     gsap.registerPlugin(ScrollTrigger);
     
     const scrollBar = Scrollbar.init(document.querySelector('.main'), {
         damping: 0.06,
         delegateTo: document,
         alwaysShowTracks: false,
-        speed: 3,
+        speed: 6,
     });
     ScrollTrigger.defaults({
         scroller: '.main',
@@ -82,37 +91,52 @@ const MainContent = () => {
   }, []);
 
   return (
-    <div className="main h-screen w-full flex flex-col overflow-auto">
-        <section className="min-h-screen w-screen relative flex flex-col items-center justify-center font-display px-64"
-        data-bgcolor="#b5ada5"
-        data-textcolor="#000000">
-            <div className="card">
-                <h1 className="first-load text-6xl">Hello, my name is:</h1>
-                <div className="reveal overflow-hidden">
-                    <h1 className="second-load text-8xl mt-7">Jun Simons</h1>
+    <body class="min-h-screen bg-[#1f1f1f]">
+        <div className="main h-screen w-full overflow-auto">
+            <section className="min-h-screen w-screen relative flex flex-col items-center justify-center font-display px-64"
+            data-bgcolor="#ebe1d8"
+            data-textcolor="#000000">
+                {/* <div className="card bg-[#b5ada5] flex flex-col items-center justify-center min-w-[600px] min-h-[500px]"> */}
+                    <h1 className="first-load  text-6xl">Hello, my name is:</h1>
+                    <div className="reveal overflow-hidden">
+                        <h1 className="second-load text-8xl mt-7">Jun Simons</h1>
+                    </div>
+                    <hr className="first-load w-[480px] h-[3px] mt-1 bg-black border-0"></hr>
+                {/* </div> */}
+            </section>
+            <section className="min-h-screen w-screen relative flex flex-col items-center  px-32"
+            data-bgcolor="#1f1f1f"
+            data-textcolor="#ebe8e6">
+                <div className="w-3/5">
+                    <h2 className="font-display-lite text-4xl text-left leading-12">
+                        I'm a sophomore at Rensselaer Polytechnic Institute studying Computer Science, 
+                        where I'm <b>focusing on my fundamentals</b>
+                    </h2>
+                    <h2 className="mt-10 font-display-lite text-4xl text-left leading-12">
+                        At <b>Johnson and Johnson</b> I built containerized web apps, optimized HPC clusters, reduced AWS spending,
+                        and implemented auto-QC for digital pathology images.
+                    </h2>
+                    <h2 className="mt-10 font-display-lite text-4xl text-left leading-12">
+                        <b>Top Skills:</b> Python, React, C++, AWS, Docker, Kubernetes, Git
+                    </h2>
                 </div>
-                <hr className="first-load w-[480px] h-[3px] mt-1 bg-black border-0"></hr>
-            </div>
-        </section>
-        <section className="min-h-screen w-screen relative flex items-center justify-center px-32"
-        data-bgcolor="#1f1f1f"
-        data-textcolor="#ebe8e6">
-            <h2>About Me</h2>
-            {/* Your About Me content goes here */}
-        </section>
+                
+                {/* Your About Me content goes here */}
+            </section>
 
-        <section className="min-h-screen w-screen relative flex items-center justify-centern px-32"
-        data-bgcolor="#070707"
-        data-textcolor="#ffffff">
-            <h2>Projects</h2>
-            {/* Your Projects content goes here */}
-        </section>
-        <section className="min-h-screen w-screen relative flex items-center justify-center px-32"
-        data-bgcolor="#070707"
-        data-textcolor="#ffffff">
-            <h1 className="text-2xl font-bold">Jun Simons</h1>
-        </section>
-    </div>
+            <section className="min-h-screen w-screen relative flex  justify-center px-32"
+            data-bgcolor="#ebe1d8"
+            data-textcolor="#000000">
+                <h2 className="font-display text-6xl">Projects</h2>
+                {/* Your Projects content goes here */}
+            </section>
+            <section className="min-h-screen w-screen relative flex items-center justify-center px-32"
+            data-bgcolor="#070707"
+            data-textcolor="#ffffff">
+                <h1 className="text-2xl font-bold">Jun Simons</h1>
+            </section>
+        </div>
+    </body>
   );
 };
 
--- personal-site/tailwind.config.js
+++ personal-site/tailwind.config.js
@@ -4,13 +4,18 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      lineHeight: {
+        '12': '3rem',
+      }
+    },
     fontFamily: {
       'sans': ['ui-sans-serif', 'system-ui'],
       'serif': ['ui-serif', 'Georgia'],
       'mono': ['ui-monospace', 'SFMono-Regular'],
       'display': ['PPAcma'],
-      'body': ["Open Sans"],
+      'display-lite': ['Roslindale'],
+      'body': ["Georgia"],
     },
   },
   plugins: [],
